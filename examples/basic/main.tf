188f98e5a30d69304d63d3041bf2578e6f855e9c
Sebastian Czech <sebaczech@gmail.com>
2024-08-05 14:12:49 +0200

    feat: Use module 1.1.2-rc.1

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,6 +1,6 @@
 module "lambda" {
   source  = "sebastianczech/free-serverless-modules/aws//modules/lambda"
-  version = "1.1.2"
+  version = "1.1.2-rc.1"
 
   name          = "my-lambda"
   iam_user_name = "my-iam-user"
