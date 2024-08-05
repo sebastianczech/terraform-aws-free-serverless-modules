a7c82ce847008b0933368bfff3ea6ee993e94773
Sebastian Czech <sebaczech@gmail.com>
2024-08-05 14:27:55 +0200

    chore: Use modules in version 1.1.2

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,6 +1,6 @@
 module "lambda" {
   source  = "sebastianczech/free-serverless-modules/aws//modules/lambda"
-  version = "1.2.0"
+  version = "1.1.2"
 
   name          = "my-lambda"
   iam_user_name = "my-iam-user"
