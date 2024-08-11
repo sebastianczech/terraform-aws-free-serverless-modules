52dce4856ef5c50538251dc673c9f339b6220cf1
Sebastian Czech <sebaczech@gmail.com>
2024-08-12 00:35:25 +0200

    Extend basic example

--- examples/basic/README.md
+++ examples/basic/README.md
@@ -6,27 +6,38 @@
 | Name | Version |
 |------|---------|
 | <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 1.5.0 |
+| <a name="requirement_archive"></a> [archive](#requirement\_archive) | ~> 2.0 |
 
 ## Providers
 
-No providers.
+| Name | Version |
+|------|---------|
+| <a name="provider_archive"></a> [archive](#provider\_archive) | ~> 2.0 |
 
 ## Modules
 
 | Name | Source | Version |
 |------|--------|---------|
-| <a name="module_dynamodb"></a> [dynamodb](#module\_dynamodb) | sebastianczech/free-serverless-modules/aws//modules/dynamodb | n/a |
-| <a name="module_lambda"></a> [lambda](#module\_lambda) | sebastianczech/free-serverless-modules/aws//modules/lambda | n/a |
-| <a name="module_sns"></a> [sns](#module\_sns) | sebastianczech/free-serverless-modules/aws//modules/sns | n/a |
-| <a name="module_sqs"></a> [sqs](#module\_sqs) | sebastianczech/free-serverless-modules/aws//modules/sqs | n/a |
+| <a name="module_dynamodb"></a> [dynamodb](#module\_dynamodb) | ../../modules/dynamodb | n/a |
+| <a name="module_lambda_consumer"></a> [lambda\_consumer](#module\_lambda\_consumer) | ../../modules/lambda | n/a |
+| <a name="module_lambda_producer"></a> [lambda\_producer](#module\_lambda\_producer) | ../../modules/lambda | n/a |
+| <a name="module_sns"></a> [sns](#module\_sns) | ../../modules/sns | n/a |
+| <a name="module_sqs"></a> [sqs](#module\_sqs) | ../../modules/sqs | n/a |
 
 ## Resources
 
-No resources.
+| Name | Type |
+|------|------|
+| [archive_file.consumer](https://registry.terraform.io/providers/hashicorp/archive/latest/docs/data-sources/file) | data source |
+| [archive_file.producer](https://registry.terraform.io/providers/hashicorp/archive/latest/docs/data-sources/file) | data source |
 
 ## Inputs
 
-No inputs.
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| <a name="input_iam_username"></a> [iam\_username](#input\_iam\_username) | The name of the IAM user | `string` | n/a | yes |
+| <a name="input_mail"></a> [mail](#input\_mail) | The email address used for notifications | `string` | n/a | yes |
+| <a name="input_prefix"></a> [prefix](#input\_prefix) | The prefix for the resources | `string` | n/a | yes |
 
 ## Outputs
 
--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,32 +1,97 @@
-module "lambda" {
-  source = "sebastianczech/free-serverless-modules/aws//modules/lambda"
+# https://registry.terraform.io/providers/hashicorp/archive/latest/docs/data-sources/file
+data "archive_file" "producer" {
+  type = "zip"
+  source {
+    content = templatefile("files/producer.py", {
+      queue_url = module.sqs.id
+    })
+    filename = "producer.py"
+  }
+  output_path = "files/producer.zip"
+}
 
-  name          = "my-lambda"
-  iam_user_name = "my-iam-user"
+# https://registry.terraform.io/providers/hashicorp/archive/latest/docs/data-sources/file
+data "archive_file" "consumer" {
+  type = "zip"
+  source {
+    content = templatefile("files/consumer.py", {
+      topic_url = module.sns.id,
+      table_url = module.dynamodb.id
+    })
+    filename = "consumer.py"
+  }
+  output_path = "files/consumer.zip"
+}
+
+module "lambda_producer" {
+  # source = "sebastianczech/free-serverless-modules/aws//modules/lambda"
+  source = "../../modules/lambda"
+
+  name          = "${var.prefix}-lambda-producer"
+  iam_user_name = var.iam_username
+
+  filename = data.archive_file.producer.output_path
+
   sqs = {
-    arn = "arn:aws:sqs:us-east-1:123456789012:my-sqs"
-    url = "https://sqs.us-east-1.amazonaws.com/123456789012/my-sqs"
+    enabled = true
+    arn     = module.sqs.arn
+  }
+
+  sns = {
+    enabled = false
+  }
+
+  dynamodb = {
+    enabled = false
+  }
+}
+
+module "lambda_consumer" {
+  # source = "sebastianczech/free-serverless-modules/aws//modules/lambda"
+  source = "../../modules/lambda"
+
+  name          = "${var.prefix}-lambda-consumer"
+  iam_user_name = var.iam_username
+
+  filename = data.archive_file.consumer.output_path
+
+  sqs = {
+    enabled = true
+    arn     = module.sqs.arn
+  }
+
+  sns = {
+    enabled = true
+    arn     = module.sns.arn
+  }
+
+  dynamodb = {
+    enabled = true
+    arn     = module.dynamodb.arn
   }
 }
 
 module "dynamodb" {
-  source = "sebastianczech/free-serverless-modules/aws//modules/dynamodb"
+  # source = "sebastianczech/free-serverless-modules/aws//modules/dynamodb"
+  source = "../../modules/dynamodb"
 
-  name           = "my-dynamodb"
+  name           = "${var.prefix}-dynamodb"
   read_capacity  = 5
   write_capacity = 5
 }
 
 module "sns" {
-  source = "sebastianczech/free-serverless-modules/aws//modules/sns"
+  # source = "sebastianczech/free-serverless-modules/aws//modules/sns"
+  source = "../../modules/sns"
 
-  name     = "my-sns"
+  name     = "${var.prefix}-sns"
   protocol = "email"
-  endpoint = "example@example.com"
+  endpoint = var.mail
 }
 
 module "sqs" {
-  source = "sebastianczech/free-serverless-modules/aws//modules/sqs"
+  # source = "sebastianczech/free-serverless-modules/aws//modules/sqs"
+  source = "../../modules/sqs"
 
-  name = "my-sqs"
+  name = "${var.prefix}-sqs"
 }
